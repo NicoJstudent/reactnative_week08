5e4b0a4f1b87df258a7c9204449c784a20f6a5d1
NicoJstudent <127749524+NicoJstudent@users.noreply.github.com>
2024-03-01 09:43:33 +0200

    .env

---
+++ firestore/firebase/Config.js
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+
+
+initializeApp(firebaseConfig);
+
+const firestore = getFirestore();
+
+const MESSAGES = 'messages';
+
+export {
+    firestore,
+    collection,
+    addDoc,
+    serverTimestamp,
+    MESSAGES
+};
